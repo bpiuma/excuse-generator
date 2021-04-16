b269d47bd0a6ab2c2e3e343f21bb8ad7f81bad0d
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-04-16 13:11:07 +0000

    Subo version 2

--- src/app.js
+++ src/app.js
@@ -6,10 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-  let personas = ["Pablo", "Luciana"];
-  console.log(personas);
-  console.log(personas[0]);
+  let whoIndex = Math.floor(Math.random() * 4);
+  let actionIndex = Math.floor(Math.random() * 4);
+  let whatIndex = Math.floor(Math.random() * 3);
+  let whenIndex = Math.floor(Math.random() * 5);
+
+  this.console.log(
+    who[whoIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
+  );
 };
